beccb06ea364db6ba90c3e1a2b13f3e4df9e4077
Evgeniy Andreev <7253968@gmail.com>
2026-03-30 10:36:09 +0300

    Добавлен логгер

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module Production_Ready_HTTP_Server_Code
 
 go 1.25.0
+
+require (
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/zerolog v1.35.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,45 @@
 package logger
+
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/pkgerrors"
+)
+
+type Logger struct {
+	zerolog.Logger
+}
+
+// New создает новый экземпляр логгера с настройками для продакшена
+func New(serviceName string, environment string) *Logger {
+	// Настраиваем формат времени
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+
+	// Включаем поддержку ошибок со стектрейсом
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+
+	// Определяем уровень логирования
+	logLevel := zerolog.InfoLevel 
+	if environment == "development" {
+		logLevel = zerolog.DebugLevel 
+	}
+
+	// Создаем контекст логгера с глобальными полями
+	ctx := zerolog.New(os.Stdout).
+		Level(logLevel).
+		With().
+		Timestamp().
+		Str("service", serviceName).
+		Str("env", environment).
+		Logger()
+
+	return &Logger{ctx}
+}
+
+// Fatal логирует фатальную ошибку и завершает программу
+func (l *Logger) Fatal(err error, msg string) {
+	l.Error().Stack().Err(err).Msg(msg)
+	os.Exit(1)
+}
